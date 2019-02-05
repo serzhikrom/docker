0e08f48d9091c5694c7a47391248155a8b9b0418
devprom-dev <devprom-dev>
2019-02-05 16:57:21 +0300

    postfix added

--- README.md
+++ README.md
@@ -1,29 +1,15 @@
-### Ubuntu
+### Ubuntu/Debian
 
 ```
 sudo -s
-apt-get update && apt-get -y install curl docker-compose git && (curl -sSL https://get.docker.com | sh)
-mkdir /etc/devprom && cd /etc/devprom && git clone https://github.com/devprom-dev/docker.git ./
-docker-compose -f ./compose.yaml up -d
-
-docker run -d --name alm --restart unless-stopped -p 80:80 -p 3306:3306 --cap-add=SYS_ADMIN --cap-add=NET_ADMIN --net=host -v /var/www/devprom:/var/www/devprom -v /etc/php/7.0/apache2/conf.d/devprom.ini:/home/devprom/docker/php/devprom.ini -v /etc/mysql/conf.d/devprom.cnf:/home/devprom/docker/mysql/devprom.cnf -v /etc/apache2/sites-available/000-default.conf:/home/devprom/docker/apache2/devprom.conf -v /etc/localtime:/etc/localtime:ro devprom/alm:latest
+apt-get update && apt-get -y install docker.io
+docker run -d --name alm --restart unless-stopped -p 80:80 -p 3306:3306 --env MYSQL_ROOT_PASSWORD=devprom_pass --cap-add=SYS_ADMIN --cap-add=NET_ADMIN --net=host -v $(pwd)/backup:/var/www/devprom/backup -v /etc/localtime:/etc/localtime:ro devprom/alm:latest
+chown -R www-data:www-data ./
 ```
 
 ### CentOS/RedHat
 
 ```
 yum -y install curl docker-compose git && (curl -sSL https://get.docker.com | sh)
-mkdir /etc/devprom && cd /etc/devprom && git clone https://github.com/devprom-dev/docker.git ./
-docker-compose -f ./compose.yaml up -d
-```
-
-### Debian
-
-```
-apt-get update && apt-get -y install curl git 
-curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-curl -sSL https://get.docker.com | sh
-mkdir /etc/devprom && cd /etc/devprom && git clone https://github.com/devprom-dev/docker.git ./
-docker-compose -f ./compose.yaml up -d
+docker run -d --name alm --restart unless-stopped -p 80:80 -p 3306:3306 --env MYSQL_ROOT_PASSWORD=devprom_pass --cap-add=SYS_ADMIN --cap-add=NET_ADMIN --net=host -v $(pwd)/backup:/var/www/devprom/backup -v /etc/localtime:/etc/localtime:ro devprom/alm:latest
 ```
--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ cat << _EOF_ > Dockerfile
 FROM debian:latest
 MAINTAINER Evgeny Savitsky <evgeny.savitsky@devprom.ru>
 
-# Base layer
+#
 ENV CROSS_COMPILE=/usr/bin/
 
 #
@@ -21,12 +21,30 @@ RUN service mysql start && mysqladmin -u root password $MYSQL_ROOT_PASSWORD && \
   mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO devprom@localhost WITH GRANT OPTION"
 
 #
+RUN apt-get -y update && apt-get -y install tzdata apt-utils rsyslog
+
+ENV DEBIAN_FRONTEND noninteractive
+RUN apt-get install -y postfix sasl2-bin && \
+  postconf -e "mydestination = localhost" && \
+  postconf -e "myhostname = devprom.local" && \
+  postconf -e "smtpd_sasl_auth_enable=yes" && \
+  postconf -e "broken_sasl_auth_clients=yes" && \
+  postconf -e "smtpd_relay_restrictions=permit_sasl_authenticated,reject_unauth_destination" && \
+  postconf -e "smtpd_sasl_security_options = noanonymous" && \
+  echo noreply | saslpasswd2 -c -p -u devprom.local noreply && \
+  ln  /etc/sasldb2 /var/spool/postfix/etc/sasldb2 && \
+  adduser postfix sasl && \
+  touch /var/log/mail.log
+
+COPY smtp/smtpd.conf /etc/postfix/sasl/smtpd.conf
+
+#
 RUN echo "* * * * * root /usr/bin/php /var/www/devprom/htdocs/core/processjobs.php >/dev/null 2>&1" >>  /etc/crontab
 RUN echo "" >>  /etc/crontab
 
 #
-RUN mkdir -p /var/www/devprom && mkdir /var/www/devprom/backup && mkdir /var/www/devprom/update && \
-  mkdir /var/www/devprom/files && mkdir /var/www/devprom/logs && mkdir /var/www/devprom/mysql
+RUN mkdir -p /var/www/devprom && mkdir /var/www/devprom/backup  && mkdir /var/www/devprom/update && \
+  mkdir /var/www/devprom/files && mkdir /var/www/devprom/logs
 
 #
 RUN wget -O /var/www/devprom/devprom.zip https://myalm.ru/download/devprom-zip && \
@@ -34,12 +52,24 @@ RUN wget -O /var/www/devprom/devprom.zip https://myalm.ru/download/devprom-zip &
   chown -R www-data:www-data /var/www/devprom && chmod -R 755 /var/www/devprom && rm -f /var/www/devprom/*.sh
 
 #
-VOLUME /var/www/devprom
-VOLUME /etc/php/7.0/apache2/conf.d/devprom.ini
-VOLUME /etc/mysql/conf.d/devprom.cnf
-VOLUME /etc/apache2/sites-available/000-default.conf
+VOLUME /var/www/devprom/backup
 
-CMD (set -e && rm -f /usr/local/apache2/logs/httpd.pid && exec httpd -DFOREGROUND)
+#
+RUN rm /etc/apache2/sites-available/* && rm /etc/apache2/sites-enabled/*
+COPY php/devprom.ini /etc/php/7.0/apache2/conf.d/
+COPY mysql/devprom.cnf /etc/mysql/conf.d/
+COPY app/settings.yml /var/www/devprom/htdocs/co/bundles/Devprom/ApplicationBundle/Resources/config/settings.yml 
+COPY apache2/devprom.conf /etc/apache2/sites-available/
+RUN a2ensite devprom.conf
+
+CMD ( set -e && \
+  service cron start && \
+  service rsyslog start && \
+  service postfix start && \
+  service mysql start && \
+  export APACHE_RUN_USER=www-data && export APACHE_RUN_GROUP=www-data && export APACHE_PID_FILE=/var/run/apache2/.pid && \
+  export APACHE_RUN_DIR=/var/run/apache2 && export APACHE_LOCK_DIR=/var/lock/apache2 && export APACHE_LOG_DIR=/var/log/apache2 && \
+  exec apache2 -DFOREGROUND )
 _EOF_
 
 docker pull debian:latest
