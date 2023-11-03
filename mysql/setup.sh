2e564c7fefd15a381628821b2cf9f633c7e5bf42
devprom-dev <devprom-dev@users.noreply.github.com>
2023-11-03 11:01:32 +0300

    Update setup.sh

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -1,2 +1,2 @@
-mysql -u root --password=$MYSQL_ROOT_PASSWORD -e "CREATE USER devprom@localhost IDENTIFIED BY '$MYSQL_PASSWORD'"
+mysql -u root --password=$MYSQL_ROOT_PASSWORD -e "CREATE USER 'devprom'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
 mysql -u root --password=$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'devprom'@'%' WITH GRANT OPTION"
