7d48b53bee4ba25778a62e361fd3ee2e2095f07b
devprom-dev <devprom-dev@users.noreply.github.com>
2023-11-03 12:27:36 +0300

    Update build-cron.sh

--- build-cron.sh
+++ build-cron.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
+cat << _EOF_ > Dockerfile
 FROM alpine:latest
+MAINTAINER Evgeny Savitsky <evgeny.savitsky@devprom.ru>
 
 RUN apt-get update && apt-get -y install cron curl
 
 RUN echo '* * * * * root curl -L -m 1800 -k "http://127.0.0.1/tasks/command.php?class=runjobs"' >>  /etc/crontab
 
 CMD ["cron", "-f"]
+_EOF_
+
+docker pull alpine:latest
+docker build -t devprom/alm-cron:latest .
+docker push devprom/alm-cron:latest
